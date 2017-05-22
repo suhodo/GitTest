d5121161953802d255526407b5edb350b631d5e3
SVN_Server\suhodo <humanature@daum.net>
2017-05-22 20:06:09 +0900

    scanf

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+	int num1=0, num2=0;
 	printf("HelloWorld\n");
 	printf("Good Morning\n");
 	printf("Bye");
@@ -9,7 +10,7 @@ int main()
 	puts("Qoooooooooo");
 
 	printf("2*4=%d", 2*4);
-
+	scanf("%d%d", &num1, &num2);
 
 
 	return 0;
